8757d730efb9ae852db84404cd03fb836d898708
Máta <70514831+Fireroth@users.noreply.github.com>
2022-11-19 14:51:32 +0100

    Add files via upload

--- scripts/externalMain.js
+++ scripts/externalMain.js
@@ -3,6 +3,8 @@ var dMode = "off";
 document.getElementById("all").style.filter = filter;
 
 console.log("%cHi!", "color:#EB5E28; font-size: 50px; text-shadow: 3px 3px #3f3e3b , 6px 6px #302f2f , 9px 9px #181818 ;");
+console.log("JQuery version: 3.6.0")
+console.log("Website version: 2.91")
 
 
 document.addEventListener("keydown", e => {
@@ -10,52 +12,6 @@ document.addEventListener("keydown", e => {
     if (dMode == "off") {document.designMode = "on"; console.log("designMode = on"); dMode = "on"}
     else {document.designMode = "off"; console.log("designMode = off"); dMode = "off"}}
   }, false);
-
-
-document.addEventListener('keydown', (event) => {
-  var name = event.key;
-  if (name === 'K') {
-    $("#all").css("filter", "invert()");
-    console.log("Filter = invert");
-    return;
-  }
-}, false);
-
-document.addEventListener('keydown', (event) => {
-  var name = event.key;
-  if (name === 'O') {
-    $("#all").css("filter", "none")
-    console.log("Filter = none");;
-    return;
-  }
-}, false);
-
-document.addEventListener('keydown', (event) => {
-  var name = event.key;
-  if (name === 'P') {
-    $("#all").css("filter", "hue-rotate(180deg)");
-    console.log("Filter = hue rotation 180deg");
-    return;
-  }
-}, false);
-
-document.addEventListener('keydown', (event) => {
-  var name = event.key;
-  if (name === 'U') {
-    $("#all").css("filter", "hue-rotate(90deg)");
-    console.log("Filter = hue rotation 90deg");
-    return;
-  }
-}, false);
-
-document.addEventListener('keydown', (event) => {
-  var name = event.key;
-  if (name === 'L') {
-    $("#all").css("filter", "hue-rotate(240deg)");
-    console.log("Filter = hue rotation 240deg");
-    return;
-  }
-}, false);
 
 //------------------------------------------------------------
 
