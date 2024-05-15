6a082c99bc8185e6a35847a3a87e9433c41e994a
Máta <70514831+Fireroth@users.noreply.github.com>
2024-05-15 20:09:23 +0200

    Delete scripts directory

--- scripts/alert.js
+++
@@ -1,3 +0,0 @@
-function alertDS() {alert("Discord server is under construction!");}
-function alertUnavailable() {alert("Target is currently unavailable");}
-function alertTest() {alert("---");}
--- scripts/cookies.js
+++
@@ -1,4 +0,0 @@
-//document.cookie = "username=001";
-
-
-//document.cookie = "filter=none";
