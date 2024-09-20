5c8c7a28fa1fa24c95380017ebd67b20d1659e2b
Máta <70514831+Fireroth@users.noreply.github.com>
2024-09-20 22:49:50 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,3 +3,30 @@ console.log("JQuery version: 3.6.0")
 
 function alertDS() {alert("Discord server is under construction!");}
 function alertUnavailable() {alert("Target is currently unavailable");}
+
+var defaultTheme = localStorage.getItem('theme');
+if(null === defaultTheme)
+{
+    localStorage.setItem('theme', 'light');
+}
+
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const body = document.body;
+
+const theme = localStorage.getItem('theme');
+
+if (theme) {
+    body.classList.add(theme);
+  }
+
+darkButton.onclick = () => {
+    body.classList.replace('light', 'dark');
+    localStorage.setItem('theme', 'dark');
+  };
+  
+  lightButton.onclick = () => {
+    body.classList.replace('dark', 'light');
+  
+    localStorage.setItem('theme', 'light');
+  };
