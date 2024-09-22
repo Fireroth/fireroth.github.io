d5cc140237f0c1dd4fda4ab848c3c80886009e6b
Máta <70514831+Fireroth@users.noreply.github.com>
2024-09-22 21:15:13 +0200

    updated theme switcher

--- js/main.js
+++ js/main.js
@@ -1,32 +1,38 @@
-console.log("%cHello!", "color:#725AC1; font-size: 50px; text-shadow: 3px 3px #3f3e3b , 6px 6px #302f2f , 9px 9px #181818 ;");
-console.log("JQuery version: 3.6.0")
+console.log(
+  "%cHello!",
+  "color:#725AC1; font-size: 50px; text-shadow: 3px 3px #3f3e3b , 6px 6px #302f2f , 9px 9px #181818 ;"
+);
+console.log("JQuery version: 3.6.0");
 
-function alertDS() {alert("Discord server is under construction!");}
-function alertUnavailable() {alert("Target is currently unavailable");}
+function alertDS() {
+  alert("Discord server is under construction!");
+}
+function alertUnavailable() {
+  alert("Target is currently unavailable");
+}
 
-var defaultTheme = localStorage.getItem('theme');
-if(null === defaultTheme)
-{
-    localStorage.setItem('theme', 'light');
+var defaultTheme = localStorage.getItem("theme");
+if (null === defaultTheme) {
+  localStorage.setItem("theme", "dark");
 }
 
 const darkButton = document.getElementById('dark');
-const lightButton = document.getElementById('light');
 const body = document.body;
 
-const theme = localStorage.getItem('theme');
+const theme = localStorage.getItem("theme");
 
 if (theme) {
-    body.classList.add(theme);
-  }
+  body.classList.add(theme);
+}
 
 darkButton.onclick = () => {
-    body.classList.replace('light', 'dark');
-    localStorage.setItem('theme', 'dark');
-  };
-  
-  lightButton.onclick = () => {
-    body.classList.replace('dark', 'light');
-  
-    localStorage.setItem('theme', 'light');
-  };
+  if (localStorage.getItem("theme") == "dark") {
+    body.classList.replace("dark", "light");
+    localStorage.setItem("theme", "light");
+  }
+
+  else{
+    body.classList.replace("light", "dark");
+    localStorage.setItem("theme", "dark");
+  }
+};
